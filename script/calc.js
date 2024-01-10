90b336f460d6895b7bb50e1ba111b63d2b85f0a1
sangsoo <rlatkdtn37@naver.com>
2024-01-10 09:58:20 +0900

    체중계 완성

---
+++ script/calc.js
@@ -0,0 +1,5 @@
+const height = document.querySelector('#height')
+const weight = document.querySelector('#weight')
+const goals = document.querySelector('#goals')
+const result = document.querySelector('#result')
+console.log(height,weight,goals,result)
